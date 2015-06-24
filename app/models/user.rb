55f34616f8005ecdffcca4e92af5eb2dfed8324e
Chad Crawford <ccrawforduwlax@gmail.com>
2015-06-24 18:43:19 -0500

    add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
+
+  validates_presence_of :first_name
+  validates_presence_of :last_name
+  validates_presence_of :position
+  validates_presence_of :organization
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,14 @@
+require 'faker'
+
 FactoryGirl.define do
   factory :user do
-    
+    first_name             { Faker::Name.first_name }
+    last_name              { Faker::Name.last_name }
+    position               'Advisor'
+    organization           { Faker::Company.name }
+    email                  { Faker::Internet.email }
+    password               { Faker::Internet.password }
+
   end
 
 end
