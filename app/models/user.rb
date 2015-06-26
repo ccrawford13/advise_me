86cce6eaa2e83a08b287337eb3b82f0c412a5c80
Chad Crawford <ccrawforduwlax@gmail.com>
2015-06-26 13:35:20 -0500

    add full_name method to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,8 @@ class User < ActiveRecord::Base
   validates_presence_of :last_name
   validates_presence_of :position
   validates_presence_of :organization
+
+  def full_name
+    [first_name, last_name].join(" ")
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,11 @@ RSpec.describe User, type: :model do
   it { should validate_presence_of :last_name }
   it { should validate_presence_of :position }
   it { should validate_presence_of :organization }
+
+  describe '#full_name' do
+    it 'returns full name' do
+      user = build(:user, first_name: 'Bill', last_name: 'Advisor')
+      expect(user.full_name).to eq 'Bill Advisor'
+    end
+  end
 end
