3c045ca2273eb42aa7d7dd675389345a7691e426
Chad Crawford <ccrawforduwlax@gmail.com>
2015-06-27 01:52:46 -0500

    refactor after_sign_in_path method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def after_sign_in_path_for(resource_or_scope)
-    user_path(resource)
+    user_path(current_user)
   end
 end
