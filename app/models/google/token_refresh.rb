3f7f47bea39dac004ee2727d1cff90406732175a
Chad Crawford <ccrawforduwlax@gmail.com>
2015-07-11 01:31:53 -0500

    Fix token refresh to convert expires_in to minutes
    token refresh should run before every API call, and if token is
    expired, a new token will be retrieved using the refresh_token

--- app/models/google/token_refresh.rb
+++ app/models/google/token_refresh.rb
@@ -10,7 +10,7 @@ class TokenRefresh
   def validate_token_auth
     if expired_token?
       # if the token is expired make a post call to google with the
-      # refresh_token to recieve a new short term auth_token
+      # refresh_token to recieve a new short term auth_token(60 min)
       response = RestClient.post "https://www.googleapis.com/oauth2/v3/token",
                                   client_id: ENV["GOOGLE_CLIENT_ID"],
                                   client_secret: ENV["GOOGLE_CLIENT_SECRET"],
@@ -18,7 +18,10 @@ class TokenRefresh
                                   grant_type: 'refresh_token'
       refresh_hash = JSON.parse(response.body)
       new_token = refresh_hash['access_token']
-      new_expiration = refresh_hash['expires']
+      # divide new_expiration by 60 to get time till
+      # expiration in minutes. Each new token should
+      # be good for 60 min
+      new_expiration = refresh_hash['expires_in'] / 60
       # used explicit return statement to clarify
       # what values are being returned & why
       return new_token, new_expiration
@@ -27,12 +30,12 @@ class TokenRefresh
     else
       return @auth_token, @token_expiration
       # if the token was not expired, return
-      # the original values
+      # the original values to be rechecked at next call
     end
   end
 
   def expired_token?
-    # the token is expired if there is 1 min or less remaining
+    # treat token as expired if there is 1 min or less remaining
     # before expiration. this will be checked before every
     # call to the calendar API
     return true if @token_expiration.to_i <= 1
