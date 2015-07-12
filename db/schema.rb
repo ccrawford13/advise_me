130671b5b403191f36fa1a7bf8c18213cdfcaa00
Chad Crawford <ccrawforduwlax@gmail.com>
2015-07-12 00:04:30 -0500

    Add appointment model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150710081104) do
+ActiveRecord::Schema.define(version: 20150712032425) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "appointments", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "summary"
+    t.string   "description"
+    t.string   "start_time"
+    t.string   "end_time"
+    t.string   "attendees",   default: [],              array: true
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
+
+  add_index "appointments", ["attendees"], name: "index_appointments_on_attendees", using: :btree
+  add_index "appointments", ["user_id"], name: "index_appointments_on_user_id", using: :btree
 
   create_table "students", force: :cascade do |t|
     t.integer  "user_id"
@@ -64,5 +78,6 @@ ActiveRecord::Schema.define(version: 20150710081104) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "appointments", "users"
   add_foreign_key "students", "users"
 end
