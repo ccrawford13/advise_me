356c46c9a63ad06ef0e5eb64394a515ca671e64f
Chad Crawford <ccrawforduwlax@gmail.com>
2015-07-15 17:30:44 -0500

    Add basic validations to db persistent appointments

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,3 +1,6 @@
 class Appointment < ActiveRecord::Base
   belongs_to :user
+
+  validates_presence_of :summary
+  validates_presence_of :description
 end
--- spec/factories/appointments.rb
+++ spec/factories/appointments.rb
@@ -1,6 +1,9 @@
+require "faker"
+
 FactoryGirl.define do
   factory :appointment do
-    
+    summary               { Faker::Lorem.sentence }
+    description           { Faker::Lorem.sentence }
   end
-
+  
 end
