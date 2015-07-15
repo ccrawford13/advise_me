818bdae1e8cc81b5cd321147b032b0af904ab26d
Chad Crawford <ccrawforduwlax@gmail.com>
2015-07-15 17:49:32 -0500

    Remove extra space between factories
    This is adhering to Ruby style guide, however, I feel it compromises
    readability. Is there a performance cost of the additional whitespace?

--- spec/factories/appointments.rb
+++ spec/factories/appointments.rb
@@ -2,8 +2,8 @@ require "faker"
 
 FactoryGirl.define do
   factory :appointment do
-    summary               { Faker::Lorem.sentence }
-    description           { Faker::Lorem.sentence }
+    summary { Faker::Lorem.sentence }
+    description { Faker::Lorem.sentence }
   end
-  
+
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,12 +2,12 @@ require "faker"
 
 FactoryGirl.define do
   factory :user do
-    first_name         { Faker::Name.first_name }
-    last_name          { Faker::Name.last_name }
-    position           "Advisor"
-    organization       { Faker::Company.name }
-    email              { Faker::Internet.email("#{last_name}") }
-    password           { Faker::Internet.password }
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    position "Advisor"
+    organization { Faker::Company.name }
+    email { Faker::Internet.email("#{last_name}") }
+    password { Faker::Internet.password }
   end
 
 end
