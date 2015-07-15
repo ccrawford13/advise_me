fff6014c40c72647d19d7846746b25b795105244
Chad Crawford <ccrawforduwlax@gmail.com>
2015-07-15 17:30:17 -0500

    Adjust style consistency of factories

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,14 +1,13 @@
-require 'faker'
+require "faker"
 
 FactoryGirl.define do
   factory :user do
-    first_name             { Faker::Name.first_name }
-    last_name              { Faker::Name.last_name }
-    position               'Advisor'
-    organization           { Faker::Company.name }
-    email                  { Faker::Internet.email("#{last_name}") }
-    password               { Faker::Internet.password }
-
+    first_name         { Faker::Name.first_name }
+    last_name          { Faker::Name.last_name }
+    position           "Advisor"
+    organization       { Faker::Company.name }
+    email              { Faker::Internet.email("#{last_name}") }
+    password           { Faker::Internet.password }
   end
 
 end
