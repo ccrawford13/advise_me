cb8c102ae074d4215142c7cec29527dcedba0ff9
Chad Crawford <ccrawforduwlax@gmail.com>
2015-07-17 16:37:26 -0500

    Add faker note factory data

--- spec/factories/notes.rb
+++ spec/factories/notes.rb
@@ -1,6 +1,10 @@
+require "faker"
+
 FactoryGirl.define do
   factory :note do
-    
+    body { Faker::Lorem.paragraph }
+    date { Faker::Date.between(10.days.ago, Date.today) }
+    student
   end
-
+  
 end
